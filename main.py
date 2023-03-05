d8a52802a896f973031c348068664d91a77eac1e
Houjun Liu <houjun@jemoka.com>
2023-03-05 10:29:37 -0800

    seeding repository

---
+++ main.py
@@ -0,0 +1,20 @@
+"""
+main.py
+An example CNN implementation for the CIFAR-10 task
+"""
+
+#### Imports ####
+# we begin by importing the prerequisite tools
+# PyTorch!
+import torch
+import torch.nn as nn
+# Data Ops!
+import pandas as pd
+import numpy as np
+# Images!
+from PIL import Image
+
+
+
+
+
